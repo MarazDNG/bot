63ac11f3ce94bab0fc5dfc1490f7ed084a92eda4
Maraz <smoger@email.cz>
2022-07-16 08:08:53 +0200

    refactor: redesign structure of files
    2 main modules, mu and mu_lib containing game api and subfolders with modules respectively.

--- mu_lib/mu_window/mu_window.py
+++ mu_lib/mu_window/mu_window.py
@@ -4,13 +4,29 @@ import win32con
 from PIL import Image
 
 
-def grab_coords():
+def _game_start_pixel() -> tuple:
+    """ Return starting pixel of the game."""
+    if hasattr(_game_start_pixel, "window"):
+        return _game_start_pixel.window
+
+    h = []
+
+    def winEnumHandler(hwnd, ctx):
+        if win32gui.IsWindowVisible(hwnd) and WINDOW_PARTIAL_TEXT in win32gui.GetWindowText(hwnd):
+            #print(hex(hwnd), win32gui.GetWindowText(hwnd))
+            h.append(hwnd)
+    win32gui.EnumWindows(winEnumHandler, None)
+
+    # If something fcks up, try converting GetWindowRect armgument
+    # to hexadecimal type with hex() function.
+    x, y, _, _ = win32gui.GetWindowRect(h[0])
+
+    _game_start_pixel.window = (x, y)
+    return (x, y)
+
+
+def grab_imgage_from_window(x: int, y: int, w: int, h: int) -> Image:
     """Return image with coordinates."""
-
-    left, top = game_start_pixel()
-    w = 80
-    h = 8
-
     hdesktop = win32gui.GetDesktopWindow()
     hwndDC = win32gui.GetWindowDC(hdesktop)
     mfcDC = win32ui.CreateDCFromHandle(hwndDC)
@@ -22,7 +38,7 @@ def grab_coords():
     saveDC.SelectObject(saveBitMap)
 
     result = saveDC.BitBlt((0, 0), (w, h), mfcDC,
-                           (left+110, top+36), win32con.SRCCOPY)
+                           (x, y), win32con.SRCCOPY)
 
     bmpinfo = saveBitMap.GetInfo()
     bmpstr = saveBitMap.GetBitmapBits(True)
@@ -45,5 +61,3 @@ def press():
 
 def wait():
     pass
-
-
