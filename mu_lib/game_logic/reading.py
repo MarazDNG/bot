8788dc0e0f537bf77014cbb5ec24ddb1e368174d
MarazDNG <smoger@email.cz>
2022-08-27 22:42:25 +0200

    add read_reset

--- mu_lib/game_logic/reading.py
+++ mu_lib/game_logic/reading.py
@@ -8,6 +8,12 @@ def read_coords() -> tuple:
     return memory.my_coords()
 
 
+def read_reset() -> int:
+    win_title = mu_window.get_window_title()
+    lvl_str = re.search("Reset: \d+", win_title)[0]
+    return int(lvl_str.split()[1])
+
+
 def read_lvl() -> int:
     win_title = mu_window.get_window_title()
     lvl_str = re.search("Level: \d+", win_title)[0]
