b0c67e74b044a6ce523074d1946363334253c61c
MarazDNG <smoger@email.cz>
2022-09-08 22:18:31 +0200

    feat: add decorator for measuring method exec time

--- mu_lib/game_logic/decorators.py
+++ mu_lib/game_logic/decorators.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 
 def repeated_value_counter(foo: callable):
@@ -32,5 +33,19 @@ def d_logger(foo: callable):
         ret = foo(*args, **kwargs)
         logging.debug(f"{foo.__name__} returned {ret}")
         return ret
+
+    return wrapper
+
+
+def d_perf_measure(foo: callable):
+    """
+    Decorator for measuring from the call of the function to the return of the function.
+    """
+    def wrapper(*args, **kwargs):
+        t1 = time.perf_counter()
+        ret = foo(*args, **kwargs)
+        t2 = time.perf_counter()
+        print(f"{foo.__name__} took {t2 - t1} seconds")
+        return ret, t2 - t1
 
     return wrapper
