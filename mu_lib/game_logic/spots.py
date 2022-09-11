b6729df6180d255fea45f2df7b3df601e05ab9b9
MarazDNG <smoger@email.cz>
2022-09-11 16:55:34 +0200

    feat: new maps and spots

--- mu_lib/game_logic/spots.py
+++ mu_lib/game_logic/spots.py
@@ -41,12 +41,6 @@ n_GREAT_BAHAMUTS = {
     "coords": (239, 50),
 }
 
-n_BLUE_GOLEMS = {
-    "warp": "stadium_city",
-    "min_lvl": 100,
-    "coords": (221, 166),
-}
-
 n_SAPI_DUOS = {
     "warp": "peaceswamp1",
     "min_lvl": 300,
@@ -63,4 +57,77 @@ n_GRIZZLYS_2 = {
     "warp": "elbeland3",
     "min_lvl": 50,
     "coords": (184, 75),
+}
+
+n_DARK_KNIGHTS_1 = {
+    "warp": "dungeon3",
+    "min_lvl": 70,
+    "coords": (11, 99)
+}
+
+
+n_DARK_KNIGHTS_2 = {
+    "warp": "dungeon3",
+    "min_lvl": 70,
+    "coords": (10, 122)
+}
+
+n_POISON_BULL_FIGHTERS_1 = {
+    "warp": "dungeon3",
+    "min_lvl": 70,
+    "coords": (14, 76)
+}
+
+n_POISON_BULL_FIGHTERS_2 = {
+    "warp": "dungeon3",
+    "min_lvl": 70,
+    "coords": (9, 59)
+}
+
+n_THUNDER_LICHES_1 = {
+    "warp": "dungeon3",
+    "min_lvl": 70,
+    "coords": (24, 54)
+}
+
+
+# LVL 100 SPOTS
+
+n_MUTANTS_1 = {
+    "warp": "tarkan",
+    "min_lvl": 140,
+    "coords": (149, 37),
+}
+
+n_MUTANTS_2 = {
+    "warp": "tarkan",
+    "min_lvl": 140,
+    "coords": (134, 18),
+}
+
+
+# LVL 200 SPOTS
+
+n_BLADE_HUNTERS_1 = {
+    "warp": "kantururuins2",
+    "min_lvl": 220,
+    "coords": (153, 57),
+}
+
+n_BLADE_HUNTERS_2 = {
+    "warp": "kantururuins2",
+    "min_lvl": 220,
+    "coords": (139, 42),
+}
+
+n_SPLINTER_WOLVES_1 = {
+    "warp": "kantururuins1",
+    "min_lvl": 200,
+    "coords": (39, 233),
+}
+
+n_SPLINTER_WOLVES_2 = {
+    "warp": "kantururuins1",
+    "min_lvl": 200,
+    "coords": (84, 240),
 }
