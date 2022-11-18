d1a2f1bb1b6e5e18106dc8faf048a447a9c60b7c
Maraz <smoger@email.cz>
2022-11-18 13:15:07 +0100

    convert config from py to yaml

--- mu_lib/conf/stats.py
+++ mu_lib/conf/stats.py
@@ -1,138 +1,120 @@
-from .spots import *
-
-config = {
-    "Silco": {
-        "account": {
-            "id": "Zaram",
-            "pass": "***REMOVED***",
-            "select_offset": 300,
-            "position": 1,
-        },
-        "stats": {
-            "str": "f400",
-            "agi": "r1",
-            "vit": "f1600",
-            "ene": "r2",
-        },
-        "starting_warp": "lorencia",
-        "leveling_plan": [
-            n_BUDGE_DRAGONS,
-            n_WEREWOLVES_3,
-            n_WEREWOLVES_2,
-            n_WEREWOLVES_1,
-            n_GRIZZLYS_2,
-            n_GRIZZLYS_1,
-            n_POISON_BULL_FIGHTERS_2,
-            n_DARK_KNIGHTS_2,
-            n_DARK_KNIGHTS_1,
-            n_DEATH_KNIGHTS_4,
-            n_DEATH_KNIGHTS_3,
-            n_DEATH_KNIGHTS_2,
-            n_DEATH_KNIGHTS_1,
-            n_MUTANTS_2,
-            n_MUTANTS_1 ,
-        ],
-    },
-    "Consumer": {
-        "account": {
-            "id": "Maraz",
-            "pass": "***REMOVED***",
-            "select_offset": 0,
-            "position": 1
-        },
-        "stats": {
-            "str": "r8",
-            "agi": "r3",
-            "vit": "f3000",
-            "ene": "r3",
-        },
-        "starting_warp": "lorencia",
-        "leveling_plan": [
-            n_BUDGE_DRAGONS,
-            n_WEREWOLVES_3,
-            n_WEREWOLVES_2,
-            n_WEREWOLVES_1,
-            n_POISON_BULL_FIGHTERS_1,
-            n_DARK_KNIGHTS_2,
-            n_DARK_KNIGHTS_1,
-            n_THUNDER_LICHES_1,
-            n_MUTANTS_2,
-            n_MUTANTS_1,
-            n_BLOODY_WOLVES_1,
-            n_SPLINTER_WOLVES_3,
-            n_SPLINTER_WOLVES_2,
-            n_SPLINTER_WOLVES_1,
-            # n_SAPI_UNUS1,
-            # n_SAPI_DUOS1,
-            # n_SAPI_DUOS2,
-            n_BLADE_HUNTERS_2,
-            n_BLADE_HUNTERS_1,
-            n_BERSERKERS_2,
-            n_BERSERKERS_1,
-            n_GENOCIDERS_3,
-            n_GENOCIDERS_2,
-            n_GENOCIDERS_1,
-        ]
-    },
-    "Connie": {
-        "account": {
-            "id": "Zaram1",
-            "pass": "***REMOVED***",
-            "select_offset": 150,
-            "position": 2
-        },
-        "stats": {
-            "str": "f400",
-            "agi": "r3",
-            "vit": "f600",
-            "ene": "r4",
-        },
-        "starting_warp": "elbeland",
-        "leveling_plan": [
-            n_STRANGE_RABBITS,
-            n_WEREWOLVES_3,
-            n_WEREWOLVES_2,
-            n_WEREWOLVES_1,
-            n_POISON_BULL_FIGHTERS_1,
-            n_DARK_KNIGHTS_2,
-            n_DARK_KNIGHTS_1,
-            n_DEATH_KNIGHTS_4,
-            n_DEATH_KNIGHTS_3,
-            n_DEATH_KNIGHTS_2,
-            n_DEATH_KNIGHTS_1,
-        ]
-    },
-    "Nalik": {
-        "account": {
-            "id": "Nalik",
-            "pass": "***REMOVED***",
-            "select_offset": 0,
-            "position": 1
-        },
-        "stats": {
-            "str": "f1000",
-            "agi": "r1",
-            "vit": "f2000",
-            "ene": "f10000",
-        },
-        "starting_warp": "noria",
-        "leveling_plan": [
-            n_STRANGE_RABBITS,
-            n_WEREWOLVES_3,
-            n_WEREWOLVES_2,
-            n_WEREWOLVES_1,
-            n_POISON_BULL_FIGHTERS_1,
-            n_DARK_KNIGHTS_2,
-            n_DARK_KNIGHTS_1,
-            n_DEATH_KNIGHTS_4,
-            n_DEATH_KNIGHTS_3,
-            n_DEATH_KNIGHTS_2,
-            n_DEATH_KNIGHTS_1,
-            n_BLADE_HUNTERS_2,
-            n_BLADE_HUNTERS_1,
-            n_BERSERKERS_3,
-            n_BERSERKERS_2,
-            n_BERSERKERS_1,
-        ]
-    }
-}
+# from .spots import *
+"Silco":
+  "account":
+    "id": "Zaram"
+    "pass": "***REMOVED***"
+    "select_offset": 300
+    "position": 1
+  "stats":
+    "str": "f400"
+    "agi": "r1"
+    "vit": "f1600"
+    "ene": "r2"
+  "starting_warp": "lorencia"
+  "leveling_plan":
+    - "n_BUDGE_DRAGONS"
+    - "n_WEREWOLVES_3"
+    - "n_WEREWOLVES_2"
+    - "n_WEREWOLVES_1"
+    - "n_GRIZZLYS_2"
+    - "n_GRIZZLYS_1"
+    - "n_POISON_BULL_FIGHTERS_2"
+    - "n_DARK_KNIGHTS_2"
+    - "n_DARK_KNIGHTS_1"
+    - "n_DEATH_KNIGHTS_4"
+    - "n_DEATH_KNIGHTS_3"
+    - "n_DEATH_KNIGHTS_2"
+    - "n_DEATH_KNIGHTS_1"
+    - "n_MUTANTS_2"
+    - "n_MUTANTS_1"
+"Consumer":
+  "account":
+    "id": "Maraz"
+    "pass": "***REMOVED***"
+    "select_offset": 0
+    "position": 1
+  "stats":
+    "str": "r8"
+    "agi": "r3"
+    "vit": "f2000"
+    "ene": "r3"
+  "starting_warp": "lorencia"
+  "leveling_plan":
+    - "n_BUDGE_DRAGONS"
+    - "n_WEREWOLVES_3"
+    - "n_WEREWOLVES_2"
+    - "n_WEREWOLVES_1"
+    - "n_POISON_BULL_FIGHTERS_1"
+    - "n_DARK_KNIGHTS_2"
+    - "n_DARK_KNIGHTS_1"
+    - "n_THUNDER_LICHES_1"
+    - "n_MUTANTS_2"
+    - "n_MUTANTS_1"
+    - "n_BLOODY_WOLVES_1"
+    - "n_SPLINTER_WOLVES_3"
+    - "n_SPLINTER_WOLVES_2"
+    - "n_SPLINTER_WOLVES_1"
+    # - "n_SAPI_UNUS1"
+    # - "n_SAPI_DUOS1"
+    # - "n_SAPI_DUOS2"
+    - "n_BLADE_HUNTERS_2"
+    - "n_BLADE_HUNTERS_1"
+    - "n_BERSERKERS_3"
+    - "n_BERSERKERS_2"
+    - "n_BERSERKERS_1"
+    # - "n_GENOCIDERS_3"
+    # - "n_GENOCIDERS_2"
+    # - "n_GENOCIDERS_1"
+"Connie":
+  "account":
+    "id": "Zaram1"
+    "pass": "***REMOVED***"
+    "select_offset": 150
+    "position": 2
+  "stats":
+    "str": "f400"
+    "agi": "r3"
+    "vit": "f600"
+    "ene": "r4"
+  "starting_warp": "elbeland"
+  "leveling_plan":
+    - "n_STRANGE_RABBITS"
+    - "n_WEREWOLVES_3"
+    - "n_WEREWOLVES_2"
+    - "n_WEREWOLVES_1"
+    - "n_POISON_BULL_FIGHTERS_1"
+    - "n_DARK_KNIGHTS_2"
+    - "n_DARK_KNIGHTS_1"
+    - "n_DEATH_KNIGHTS_4"
+    - "n_DEATH_KNIGHTS_3"
+    - "n_DEATH_KNIGHTS_2"
+    - "n_DEATH_KNIGHTS_1"
+"Nalik":
+  "account":
+    "id": "Nalik"
+    "pass": "***REMOVED***"
+    "select_offset": 0
+    "position": 1
+  "stats":
+    "str": "f1000"
+    "agi": "r1"
+    "vit": "f2000"
+    "ene": "f10000"
+  "starting_warp": "noria"
+  "leveling_plan":
+    - "n_STRANGE_RABBITS"
+    - "n_WEREWOLVES_3"
+    - "n_WEREWOLVES_2"
+    - "n_WEREWOLVES_1"
+    - "n_POISON_BULL_FIGHTERS_1"
+    - "n_DARK_KNIGHTS_2"
+    - "n_DARK_KNIGHTS_1"
+    - "n_DEATH_KNIGHTS_4"
+    - "n_DEATH_KNIGHTS_3"
+    - "n_DEATH_KNIGHTS_2"
+    - "n_DEATH_KNIGHTS_1"
+    - "n_BLADE_HUNTERS_2"
+    - "n_BLADE_HUNTERS_1"
+    - "n_BERSERKERS_3"
+    - "n_BERSERKERS_2"
+    - "n_BERSERKERS_1"
